247034f892ae2e5f2c28187a26e0f421e4298386
USER <USER@DESKTOP-001LUTQ>
2023-01-17 13:20:46 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int
+            Console.WriteLine();
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Matematik
+{
+	class Program
+	{
+		static void Main(string[] args) 
+		{
+			DortIslem dortIslem = new DortIslem();
+			dortIslem.Topla(5, 6);
+            dortIslem.Topla(11,24);
+            dortIslem.Topla(97, 6);
+
+
+
+            Console.WriteLine();
+		}
+
+	}  
+
+
+
+
+
+
+} 
+
